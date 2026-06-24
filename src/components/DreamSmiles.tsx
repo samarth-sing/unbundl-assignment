d878683018f76184bb07c45993f8d7262b7674ff
samarth-sing <samarthsinghal0912@gmail.com>
2026-06-24 18:45:23 +0530

    add component of DreamSmiles component

--- src/components/DreamSmiles.tsx
+++ src/components/DreamSmiles.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { rightArrow, tickIcon, whistleImg } from "../assets/images";
 
 const DreamSmiles = () => {
   return (
     <div className="dreamSmiles">
-      <div>
-        <h2>Dream smiles achieved secretly</h2>
+      <div className="dreamSmilesLeft">
+        <h2>
+          Dream smiles <br />
+          achieved secretly
+        </h2>
         <p>
           Experience the superior quality of our Whistle Aligners crafted with
           3-layer PU material. With 450+ clinics nationwide, enjoy comfortable
@@ -12,11 +16,41 @@ const DreamSmiles = () => {
         </p>
         <p>
           The pricing is different for every case. Cases with higher complexity
-          requiring more aligners and additional time and effort
-          from our dentists.
+          requiring more aligners and additional time and effort from our
+          dentists.
         </p>
       </div>
-      <div></div>
+      <div className="dreamSmilesRight">
+        <div className="productInfo">
+          <h5>Whistle Aligners</h5>
+          <p>
+            <span>₹84,000</span>
+            <br />
+            <span>
+              <label>starting at</label>
+              <label>₹47,999</label>
+            </span>
+            <br />
+            <span>inc. of all taxes</span>
+          </p>
+          <div>
+            <div>
+              <img src={tickIcon} alt="tick icon" />
+              <span>Offer valid for a limited time</span>
+            </div>
+            <div>
+              <img src={tickIcon} alt="tick icon" />
+              <span>Easy financing options</span>
+            </div>
+          </div>
+        </div>
+        <div className="productImg">
+          <img src={whistleImg} alt="whistle product image" />
+          <a>
+            Learn more <img src={rightArrow} alt="arrow-icon" />
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
