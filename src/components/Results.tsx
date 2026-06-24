f882878c8fc0687fa19eb21eb3867aab4e84dd89
samarth-sing <samarthsinghal0912@gmail.com>
2026-06-24 23:29:44 +0530

    added loader

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,45 +1,54 @@
 import { useEffect, useState } from "react";
 import { images } from "../assets/images";
+import Loader from "./Loader";
 
 const Results = () => {
   const [result, setResult] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetch("/public/resultData.json")
-      .then((res) => res.json())
-      .then((data) => setResult(data))
-      .catch((err) => console.error(err));
+    setTimeout(() => {
+      fetch("/public/resultData.json")
+        .then((res) => res.json())
+        .then((data) => setResult(data))
+        .catch((err) => console.error(err));
+      setLoading(false);
+    }, 2000);
   }, []);
 
   return (
     <div className="Results">
       <h2 className="MainHeading ResultsHeading">Results You'll Love</h2>
-      <div className="ResultsCards">
-        {result.map((data) => (
-          <div className="ResultsCard">
-            <div className="ResultCardsImg">
-              <div>
-                <img src={images[data.beforeImg]} alt="Gap Before" />
-                <span>Before</span>
+      {loading ? (
+        <Loader />
+      ) : (
+        <div className="ResultsCards">
+          {result.map((data) => (
+            <div className="ResultsCard">
+              <div className="ResultCardsImg">
+                <div>
+                  <img src={images[data.beforeImg]} alt="Gap Before" />
+                  <span>Before</span>
+                </div>
+                <div>
+                  <img src={images[data.afterImg]} alt="Gap After" />
+                  <span>After</span>
+                </div>
               </div>
-              <div>
-                <img src={images[data.afterImg]} alt="Gap After" />
-                <span>After</span>
+              <div className="ResultCardsInfo">
+                <div>
+                  <label>Concern</label>
+                  <label>{data.concern}</label>
+                </div>
+                <div>
+                  <label>Treatment Duration</label>
+                  <label>{data.treatmentDuration}</label>
+                </div>
               </div>
             </div>
-            <div className="ResultCardsInfo">
-              <div>
-                <label>Concern</label>
-                <label>{data.concern}</label>
-              </div>
-              <div>
-                <label>Treatment Duration</label>
-                <label>{data.treatmentDuration}</label>
-              </div>
-            </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
--- src/components/WhyWhistle.tsx
+++ src/components/WhyWhistle.tsx
@@ -1,30 +1,39 @@
 import { useEffect, useState } from "react";
 import { images } from "../assets/images";
+import Loader from "./Loader";
 
 const WhyWhistle = () => {
   const [whyWhistle, setWhyWhistle] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetch("/public/whyWhistleData.json")
-      .then((res) => res.json())
-      .then((data) => setWhyWhistle(data))
-      .catch((err) => console.error(err));
+    setTimeout(() => {
+      fetch("/public/whyWhistleData.json")
+        .then((res) => res.json())
+        .then((data) => setWhyWhistle(data))
+        .catch((err) => console.error(err));
+      setLoading(false);
+    }, 2000);
   }, []);
 
   return (
     <div className="WhyWhistle">
       <h2 className="MainHeading WhyWhistleHeading">Why Whistle?</h2>
-      <div className="WhyWhistleCards">
-        {whyWhistle.map((data) => (
-          <div key={data.id} className="WhyWhistleCard">
-            <img src={images[data.serviceImg]} alt="services image" />
-            <div>
-              <h5>{data.serviceName}</h5>
-              <p>{data.serviceDetail}</p>
+      {loading ? (
+        <Loader />
+      ) : (
+        <div className="WhyWhistleCards">
+          {whyWhistle.map((data) => (
+            <div key={data.id} className="WhyWhistleCard">
+              <img src={images[data.serviceImg]} alt="services image" />
+              <div>
+                <h5>{data.serviceName}</h5>
+                <p>{data.serviceDetail}</p>
+              </div>
             </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
