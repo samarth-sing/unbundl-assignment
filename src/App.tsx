30fcc929ac1dbf8070f6435fe13bfb23363e0ac8
samarth-sing <samarthsinghal0912@gmail.com>
2026-06-24 19:54:04 +0530

    added WhyWhistle component with designing

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@
 import "./App.css";
 import DreamSmiles from "./components/DreamSmiles";
 import Results from "./components/Results";
+import WhyWhistle from "./components/WhyWhistle";
 
 function App() {
   return (
     <>
       <DreamSmiles />
       <Results />
+      <WhyWhistle />
     </>
   );
 }
--- src/components/WhyWhistle.tsx
+++ src/components/WhyWhistle.tsx
@@ -1,11 +1,54 @@
-import React from 'react'
+import React from "react";
+import { whyWhistle1 } from "../assets/images";
 
 const WhyWhistle = () => {
   return (
-    <div>
-      
+    <div className="WhyWhistle">
+      <h2 className="MainHeading WhyWhistleHeading">Why Whistle?</h2>
+      <div className="WhyWhistleCards">
+        <div className="WhyWhistleCard">
+          <img src={whyWhistle1} alt="services image" />
+          <div>
+            <h5>Custom-made & invisible</h5>
+            <p>
+              Tailored for your teeth and smile with a clear, discreet
+              appearance.
+            </p>
+          </div>
+        </div>
+        <div className="WhyWhistleCard">
+          <img src={whyWhistle1} alt="services image" />
+          <div>
+            <h5>Custom-made & invisible</h5>
+            <p>
+              Tailored for your teeth and smile with a clear, discreet
+              appearance.
+            </p>
+          </div>
+        </div>
+        <div className="WhyWhistleCard">
+          <img src={whyWhistle1} alt="services image" />
+          <div>
+            <h5>Custom-made & invisible</h5>
+            <p>
+              Tailored for your teeth and smile with a clear, discreet
+              appearance.
+            </p>
+          </div>
+        </div>
+        <div className="WhyWhistleCard">
+          <img src={whyWhistle1} alt="services image" />
+          <div>
+            <h5>Custom-made & invisible</h5>
+            <p>
+              Tailored for your teeth and smile with a clear, discreet
+              appearance.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default WhyWhistle
+export default WhyWhistle;
