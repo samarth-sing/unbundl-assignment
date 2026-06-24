9588e3f8766ead3a46c6bb40c725e9dba3148094
samarth-sing <samarthsinghal0912@gmail.com>
2026-06-24 17:27:29 +0530

    added components for frames and remove default css

---
+++ src/components/DreamSmiles.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const DreamSmiles = () => {
+  return (
+    <div className="dreamSmiles">
+      <div>
+        <h2>Dream smiles achieved secretly</h2>
+        <p>
+          Experience the superior quality of our Whistle Aligners crafted with
+          3-layer PU material. With 450+ clinics nationwide, enjoy comfortable
+          treatment by expert orthodontists at House of Clove
+        </p>
+        <p>
+          The pricing is different for every case. Cases with higher complexity
+          requiring more aligners and additional time and effort
+          from our dentists.
+        </p>
+      </div>
+      <div></div>
+    </div>
+  );
+};
+
+export default DreamSmiles;
---
+++ src/components/WhyWhistle.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const WhyWhistle = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default WhyWhistle
