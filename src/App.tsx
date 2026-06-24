6c10cfcb540e20237d0bd08dc61788d5d48e9b45
samarth-sing <samarthsinghal0912@gmail.com>
2026-06-24 19:32:37 +0530

    add components of Results components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 // import { useState } from 'react'
 import "./App.css";
 import DreamSmiles from "./components/DreamSmiles";
+import Results from "./components/Results";
 
 function App() {
   return (
     <>
       <DreamSmiles />
+      <Results />
     </>
   );
 }
--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,11 +1,102 @@
-import React from 'react'
+import React from "react";
+import { GapsAfter, GapsBefore } from "../assets/images";
 
 const Results = () => {
   return (
-    <div>
-      
+    <div className="Results">
+      <h2 className="MainHeading ResultsHeading">Results You'll Love</h2>
+      <div className="ResultsCards">
+        <div className="ResultsCard">
+          <div className="ResultCardsImg">
+            <div>
+              <img src={GapsBefore} alt="Gap Before" />
+              <span>Before</span>
+            </div>
+            <div>
+              <img src={GapsAfter} alt="Gap After" />
+              <span>After</span>
+            </div>
+          </div>
+          <div className="ResultCardsInfo">
+            <div>
+              <label>Concern</label>
+              <label>Gaps</label>
+            </div>
+            <div>
+              <label>Treatment Duration</label>
+              <label>8 months</label>
+            </div>
+          </div>
+        </div>
+        <div className="ResultsCard">
+          <div className="ResultCardsImg">
+            <div>
+              <img src={GapsBefore} alt="Gap Before" />
+              <span>Before</span>
+            </div>
+            <div>
+              <img src={GapsAfter} alt="Gap After" />
+              <span>After</span>
+            </div>
+          </div>
+          <div className="ResultCardsInfo">
+            <div>
+              <label>Concern</label>
+              <label>Gaps</label>
+            </div>
+            <div>
+              <label>Treatment Duration</label>
+              <label>8 months</label>
+            </div>
+          </div>
+        </div>
+        <div className="ResultsCard">
+          <div className="ResultCardsImg">
+            <div>
+              <img src={GapsBefore} alt="Gap Before" />
+              <span>Before</span>
+            </div>
+            <div>
+              <img src={GapsAfter} alt="Gap After" />
+              <span>After</span>
+            </div>
+          </div>
+          <div className="ResultCardsInfo">
+            <div>
+              <label>Concern</label>
+              <label>Gaps</label>
+            </div>
+            <div>
+              <label>Treatment Duration</label>
+              <label>8 months</label>
+            </div>
+          </div>
+        </div>
+        <div className="ResultsCard">
+          <div className="ResultCardsImg">
+            <div>
+              <img src={GapsBefore} alt="Gap Before" />
+              <span>Before</span>
+            </div>
+            <div>
+              <img src={GapsAfter} alt="Gap After" />
+              <span>After</span>
+            </div>
+          </div>
+          <div className="ResultCardsInfo">
+            <div>
+              <label>Concern</label>
+              <label>Gaps</label>
+            </div>
+            <div>
+              <label>Treatment Duration</label>
+              <label>8 months</label>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Results
+export default Results;
