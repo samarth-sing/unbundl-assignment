806e6c4cb1c2d98ea6e0f7d75038ebb707a6d03b
samarth-sing <samarthsinghal0912@gmail.com>
2026-06-24 17:26:23 +0530

    added all images and remove default one

---
+++ src/assets/images.ts
@@ -0,0 +1,37 @@
+const whistleImg = import("./whistleImg.png");
+const whistleIcon = import("./whistleIcon.png");
+const whyWhistle1 = import("./whyWhistle1.png");
+const whyWhistle2 = import("./whyWhistle2.png");
+const whyWhistle3 = import("./whyWhistle3.png");
+const whyWhistle4 = import("./whyWhistle4.png");
+const tickIcon = import("./tickIcon.png");
+const rightArrow = import("./rightArrow.png");
+const ProtrudingTeethAfter = import("./ProtrudingTeethAfter.png");
+const ProtrudingTeethBefore = import("./ProtrudingTeethBefore.png");
+const phoneIcon = import("./phoneIcon.png");
+const OverbiteAfter = import("./OverbiteAfter.png");
+const OverbiteBefore = import("./OverbiteBefore.png");
+const GapsAfter = import("./GapsAfter.png");
+const GapsBefore = import("./GapsBefore.png");
+const CrookedTeethAfter = import("./CrookedTeethAfter.png");
+const CrookedTeethBefore = import("./CrookedTeethBefore.png");
+
+export {
+  whistleImg,
+  whistleIcon,
+  whyWhistle1,
+  whyWhistle2,
+  whyWhistle3,
+  whyWhistle4,
+  tickIcon,
+  rightArrow,
+  ProtrudingTeethAfter,
+  ProtrudingTeethBefore,
+  phoneIcon,
+  OverbiteAfter,
+  OverbiteBefore,
+  GapsAfter,
+  GapsBefore,
+  CrookedTeethAfter,
+  CrookedTeethBefore,
+};
