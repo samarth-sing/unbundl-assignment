70db59f25a4875267969dde0dba7e69c993a3693
samarth-sing <samarthsinghal0912@gmail.com>
2026-06-24 20:47:50 +0530

    added dynamic content in result component

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,99 +1,44 @@
-import React from "react";
-import { GapsAfter, GapsBefore } from "../assets/images";
+import { useEffect, useState } from "react";
+import { images } from "../assets/images";
 
 const Results = () => {
+  const [result, setResult] = useState([]);
+
+  useEffect(() => {
+    fetch("/public/resultData.json")
+      .then((res) => res.json())
+      .then((data) => setResult(data))
+      .catch((err) => console.error(err));
+  }, []);
+
   return (
     <div className="Results">
       <h2 className="MainHeading ResultsHeading">Results You'll Love</h2>
       <div className="ResultsCards">
-        <div className="ResultsCard">
-          <div className="ResultCardsImg">
-            <div>
-              <img src={GapsBefore} alt="Gap Before" />
-              <span>Before</span>
+        {result.map((data) => (
+          <div className="ResultsCard">
+            <div className="ResultCardsImg">
+              <div>
+                <img src={images[data.beforeImg]} alt="Gap Before" />
+                <span>Before</span>
+              </div>
+              <div>
+                <img src={images[data.afterImg]} alt="Gap After" />
+                <span>After</span>
+              </div>
             </div>
-            <div>
-              <img src={GapsAfter} alt="Gap After" />
-              <span>After</span>
-            </div>
-          </div>
-          <div className="ResultCardsInfo">
-            <div>
-              <label>Concern</label>
-              <label>Gaps</label>
-            </div>
-            <div>
-              <label>Treatment Duration</label>
-              <label>8 months</label>
-            </div>
-          </div>
-        </div>
-        <div className="ResultsCard">
-          <div className="ResultCardsImg">
-            <div>
-              <img src={GapsBefore} alt="Gap Before" />
-              <span>Before</span>
-            </div>
-            <div>
-              <img src={GapsAfter} alt="Gap After" />
-              <span>After</span>
+            <div className="ResultCardsInfo">
+              <div>
+                <label>Concern</label>
+                <label>{data.concern}</label>
+              </div>
+              <div>
+                <label>Treatment Duration</label>
+                <label>{data.treatmentDuration}</label>
+              </div>
             </div>
           </div>
-          <div className="ResultCardsInfo">
-            <div>
-              <label>Concern</label>
-              <label>Gaps</label>
-            </div>
-            <div>
-              <label>Treatment Duration</label>
-              <label>8 months</label>
-            </div>
-          </div>
-        </div>
-        <div className="ResultsCard">
-          <div className="ResultCardsImg">
-            <div>
-              <img src={GapsBefore} alt="Gap Before" />
-              <span>Before</span>
-            </div>
-            <div>
-              <img src={GapsAfter} alt="Gap After" />
-              <span>After</span>
-            </div>
-          </div>
-          <div className="ResultCardsInfo">
-            <div>
-              <label>Concern</label>
-              <label>Gaps</label>
-            </div>
-            <div>
-              <label>Treatment Duration</label>
-              <label>8 months</label>
-            </div>
-          </div>
-        </div>
-        <div className="ResultsCard">
-          <div className="ResultCardsImg">
-            <div>
-              <img src={GapsBefore} alt="Gap Before" />
-              <span>Before</span>
-            </div>
-            <div>
-              <img src={GapsAfter} alt="Gap After" />
-              <span>After</span>
-            </div>
-          </div>
-          <div className="ResultCardsInfo">
-            <div>
-              <label>Concern</label>
-              <label>Gaps</label>
-            </div>
-            <div>
-              <label>Treatment Duration</label>
-              <label>8 months</label>
-            </div>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
